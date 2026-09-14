c998bba54b82111c9ff993ed7b1e0b49a629b0c6
Bruno Seidi Ferreira Takaya <brunoferreira.takaya@gmail.com>
2026-09-14 09:48:44 -0300

    refactor: separa estilos das telas e componentes

---
+++ src/styles/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Exportação centralizada dos estilos do projeto
+ *
+ * À medida que cada arquivo *.styles.ts for criado,
+ * adicione o export aqui para manter tudo organizado.
+ */
+export { styles as consultaCardStyles } from "./consultaCard.styles";
+
+// Adicione abaixo conforme for criando os demais arquivos de estilo:
+export { styles as homeScreenStyles } from "./homeScreen.styles";
+export { styles as loginMedicoStyles } from "./loginMedico.styles";
+// ...
+export { styles as loginPacienteStyles } from "./loginPaciente.styles";
+export { styles as cadastroMedicoStyles } from "./cadastroMedico.styles";
+export { styles as cadastroPacienteStyles } from "./cadastroPaciente.styles";
+export { styles as perfilMedicoStyles } from "./perfilMedico.styles";
+export { styles as consultasMedicoStyles } from "./consultasMedico.styles";
+export { styles as minhasConsultasStyles } from "./minhasConsultas.styles";
+export { styles as agendarConsultaStyles } from "./agendarConsulta.styles";
+export { styles as escolhaMedicoStyles } from "./escolhaMedico.styles";
+export { styles as escolhaEspecialidadeStyles } from "./escolhaEspecialidade.styles";
